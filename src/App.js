e17418cf8e3f3df7f8515fa0dbce9000ad7efe8d
thanawatpinya <thanawatpinya@gmail.com>
2019-02-26 05:42:06 +0700

    test build to firebase

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Test push and build to firebase
           </p>
           <a
             className="App-link"
